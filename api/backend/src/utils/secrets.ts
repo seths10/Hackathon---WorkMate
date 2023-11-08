9c4108b2f19f30db1e84ab508481e924822f3c8e
Nathan Kulewoshie <kulewoshienathan@gmail.com>
2023-11-08 14:05:04 +0000

    authorize community

--- api/backend/src/utils/secrets.ts
+++ api/backend/src/utils/secrets.ts
@@ -8,3 +8,6 @@ const prod = ENVIRONMENT === "production";
 
 export const DB_URI = prod ? process.env["MONGO_URI"] : process.env["MONGO_URI_LOCAL"];
 export const secretKey = process.env["SECRET_KEY"];
+
+
+console.log(secretKey);
