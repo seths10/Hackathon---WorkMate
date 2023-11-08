57de74a4b93c35d5eece3b5c0e46f4f0ad37566f
Nathan Kulewoshie <kulewoshienathan@gmail.com>
2023-11-08 13:50:28 +0000

    mongo uri

--- api/backend/src/app.ts
+++ api/backend/src/app.ts
@@ -23,7 +23,7 @@ app.use("/api/community", auth, communityRoute);
 app.use("/api/booking", auth, bookingRoute);
 
 mongoose.Promise = bluebird;
-const mongodb_uri = DB_URI! || "mongodb+srv://nathankulewoshie:iVxRjA3id2lgwT59@workmate.7uoo1gc.mongodb.net/?retryWrites=true&w=majority";
+const mongodb_uri = DB_URI! || "mongodb://localhost:27017/workmate";
 
 // db connection
 mongoose
