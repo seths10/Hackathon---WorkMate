fb00e7bccda83f93086516380b86ce0800d6e84c
Seth Addo <59029978+seths10@users.noreply.github.com>
2023-11-09 10:14:32 +0000

    feat: updated login UI

--- client/workmate-ui/src/pages/homepage/main.tsx
+++ client/workmate-ui/src/pages/homepage/main.tsx
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
 import HomeImg from "../../assets/images/COMMUNITY.png";
 
 const HomePage = () => {
-  
   React.useEffect(() => {
     document.title = `Home | ${siteTitle}`;
   }, []);
@@ -13,16 +12,21 @@ const HomePage = () => {
   return (
     <React.Fragment>
       <HomePageLayout>
-        <main className="bg-orange-20 ">
-          <div className="hero px-[6rem] py-[2rem] flex mt-20 items-center justify-center">
+        <main>
+          <div className="hero px-[6rem] flex mt-[3.5rem] items-center justify-center">
             <div className="flex-auto w-60">
-              <h2 className="text-7xl font-extrabold">
-                Feel free to reach out to{" "}
-                <span className="text-orange-500">discuss how your mate</span>{" "}
-                can help you.
+              <h2 className="flex flex-col text-gray-800 gap-2 text-6xl font-extrabold">
+                <p>Making</p>
+                <p>Everybody Feel</p>
+                <p>
+                  Valued At Work
+                </p> <span className="text-orange-500"></span>{" "}
               </h2>
-              <p className="text-xl text-grey-500 mt-3">
-                Brings a wealth of knowledge and experience to your team.
+              <p className="text-md text-gray-500 mt-3">
+                With our solution, you can collaborate with your individuals
+              </p>
+              <p className="text-md text-gray-500 ">
+                and also books a space for yourself in the office
               </p>
               <div className="flex">
                 <Link to="/community">
@@ -36,7 +40,7 @@ const HomePage = () => {
                 </Link>{" "}
                 <Link
                   to="/desk-booking"
-                  className="mt-6 block w-[150px] select-none border-2 border-orange-500 rounded-full py-3 px-6 text-center font-sans text-lg font-medium text-black shadow-md shadow-black-500/20 transition-all hover:shadow-lg hover:shadow-black-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                  className="mt-6 block w-[150px] text-[#d65627] select-none border border-orange-500 rounded-full py-3 px-6 text-center font-sans text-lg font-medium shadow-md shadow-black-500/20 transition-all hover:shadow-lg hover:shadow-black-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                   type="button"
                   data-ripple-light="true"
                 >
@@ -46,6 +50,28 @@ const HomePage = () => {
             </div>
             <div className="flex-auto w-1 ">
               <img src={HomeImg} alt="home image" />
+            </div>
+          </div>
+
+          <div className="flex justify-content items-center mt-[4rem] gap-5 mb-20 px-[5rem]">
+            <div className="max-w-sm p-6  border-2 border-dashed border-white rounded-lg">
+              <i className="fa fa-users bg-white flex items-center justify-center rounded-lg px-2 py-2 me-2 w-9 h-9 text-gray-500 mb-3"></i>
+                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900">
+                  Community
+                </h5>
+              <p className="mb-3 font-normal text-gray-500">
+                Ask Questions and get quick answers from your workmates
+              </p>
+            </div>
+
+            <div className="max-w-sm p-6  border-2 border-dashed border-white rounded-lg">
+              <i className="fa fa-briefcase bg-white flex items-center justify-center rounded-lg px-2 py-2 me-2 w-9 h-9 text-gray-500 mb-3"></i>
+                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900">
+                  Desk Booking
+                </h5>
+              <p className="mb-3 font-normal text-gray-500">
+                Ask Questions and get quick answers from your workmates
+              </p>
             </div>
           </div>
         </main>
